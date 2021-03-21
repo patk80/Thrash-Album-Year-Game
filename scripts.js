1fd742a846beb00800d0f9cda45ced7625901ac2
Pat Kelly <PatK80@gmail.com>
2021-03-21 17:22:32 -0400

    comment for how to splice out

--- scripts.js
+++ scripts.js
@@ -16,6 +16,8 @@ function randomAlbum() {
     document.querySelector("#albumTextContent").textContent   = albumsArray[randomIndex].artist + " — " + albumsArray[randomIndex].album;
     document.querySelector("#albumArtwork").src               = albumsArray[randomIndex].imagePath;
     document.querySelector("#flagArtwork").src                = albumsArray[randomIndex].countryPath;
+    // remove - with splice
+
 };
 
 //Function to check if the user's input/answer matches the selected album's data //
