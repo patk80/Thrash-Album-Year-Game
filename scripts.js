15cd4c65c7a96b850c4e70d7070e188f537aec8c
Pat Kelly <PatK80@gmail.com>
2021-10-27 20:25:20 -0400

    Changing mdash to ndash in html/js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ function randomAlbum() {
         submitButton.style.visibility                               = "visible";
         input.style.visibility                                      = "visible";
         document.querySelector("#country-text-content").textContent = albumsArray[randomIndex].country;
-        document.querySelector("#album-text-content").textContent   = albumsArray[randomIndex].artist + " — " + albumsArray[randomIndex].album;
+        document.querySelector("#album-text-content").textContent   = albumsArray[randomIndex].artist + " – " + albumsArray[randomIndex].album;
         document.querySelector("#album-artwork").src                = albumsArray[randomIndex].imagePath;
         document.querySelector("#flag-artwork").src                 = albumsArray[randomIndex].countryPath;
         // remove current album from albumsArry with splice //
